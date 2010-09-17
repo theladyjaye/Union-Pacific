b8c7c2a56b3f41e24cb24a0fa02a410a6bfd8a29
aventurella <aventurella@gmail.com>
2010-09-17 12:09:52 -0700

    server

---
+++ application.js
@@ -0,0 +1,35 @@
+require.paths.unshift("/usr/local/lib/node");
+
+var connect      = require('connect');
+//var main         = require('./endpoints/main');
+//var matches      = require('./endpoints/matches');
+//var games        = require('./endpoints/games');
+//var sampledata   = require('./endpoints/sampledata');
+
+
+var server = connect.createServer(
+	//form({ keepExtensions: false }),
+	connect.logger({ buffer: true })
+);
+
+var vhost = connect.vhost('unionpacific', server);
+
+server.use("/", connect.staticProvider(__dirname + '/'));
+server.use("/resources/css", connect.staticProvider(__dirname + '/resources/css'));
+server.use("/resources/imgs", connect.staticProvider(__dirname + '/resources/imgs'));
+server.use("/resources/js", connect.staticProvider(__dirname + '/resources/js'));
+server.use("/resources/templates", connect.staticProvider(__dirname + '/resources/templates'));
+
+//server.use("/api/sampledata/", connect.router(sampledata.endpoints));
+//server.use("/games/", connect.router(games.endpoints));
+//server.use("/matches/", connect.router(matches.endpoints));
+
+
+//server.use(main.defaultResponse);
+//server.use(main.renderResponse);
+
+//server.use("/session/", connect.router(session.endpoints));
+
+server.listen(80);
+
+console.log('Union Pacific server listening on port 80');
